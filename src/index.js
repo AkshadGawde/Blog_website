6cbc9b96f5675e05816513d97cc36c57c7d33033
aKshad <gawdeakshad@gmail.com>
2024-06-07 22:11:18 +0530

    Routing

--- src/index.js
+++ src/index.js
@@ -4,13 +4,38 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import PostDetails from "./pages/PostDetail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import Author from "./pages/Authors";
+import CreatePost from "./pages/CreatePost";
+import Dashboard from "./pages/Dashboard";
+import EditPost from "./pages/EditPost";
+import Logout from "./pages/Logout";
+import CategoryPosts from "./pages/CategoryPosts";
+import AuthorPosts from "./pages/AuthorPosts";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: <ErrorPage />,
-    // children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "posts/:id", element: <PostDetails /> },
+      { path: "register", element: <Register /> },
+      { path: "login", element: <Login /> },
+      { path: "profile/:id", element: <UserProfile /> },
+      { path: "author", element: <Author /> },
+      { path: "posts", element: <CreatePost /> },
+      { path: "posts/categories/:category", element: <CategoryPosts /> },
+      { path: "profile/users/:id", element: <AuthorPosts /> },
+      { path: "myposts/:id", element: <Dashboard /> },
+      { path: "posts/:id/edit", element: <EditPost /> },
+      { path: "logout", element: <Logout /> },
+    ],
   },
 ]);
 
