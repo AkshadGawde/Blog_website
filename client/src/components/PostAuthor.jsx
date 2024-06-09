57ddb6c894281a929fb400a3cdd71c401ccfcb56
Akshad Gawde <gawdeakshad@gmail.com>
2024-06-09 19:10:17 +0530

    Post Author

--- client/src/components/PostAuthor.jsx
+++ client/src/components/PostAuthor.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Avatar from "../images/avatar1.jpg";
 
 const PostAuthor = () => {
-  return <div></div>;
+  return (
+    <Link to={`/posts/users/sdfsdf`}>
+      <div className="post__author">
+        <img src={Avatar} alt="" />
+      </div>
+
+      <div className="post__author-details">
+        <h5>By: Ernst Achiever</h5>
+        <small>June 23, 2021</small>
+      </div>
+    </Link>
+  );
 };
 
 export default PostAuthor;
