a4bcbae0e6d7aa16928d6f4e635a99d92fcd80d6
Akshad Gawde <gawdeakshad@gmail.com>
2024-06-12 12:46:51 +0530

    Register form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return <div>Register</div>;
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const changeInputHandler = (e) => {
+    setUserData((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+  return (
+    <section className="register">
+      <div className="container">
+        <h2>SignUp</h2>
+        <form className="form register__form">
+          <p className="form__error-message">This is an error message</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            value={userData.name}
+            onChange={changeInputHandler}
+            autoFocus
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={userData.name}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <input
+            type="password"
+            name="password2"
+            placeholder="Confirm Password"
+            value={userData.password}
+            onChange={changeInputHandler}
+          />
+          <button type="submit" className="btn primary">
+            {" "}
+            Register{" "}
+          </button>
+        </form>
+        <small>
+          Already have an account ? <Link to="/login">Sign in</Link>
+        </small>
+      </div>
+    </section>
+  );
 };
 
 export default Register;
